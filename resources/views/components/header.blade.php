90431b2e2d249bccf8ea81c07edd599740dafbb5
Fabio Zhang <Flasvert@gmail.com>
2023-05-11 20:13:56 +0200

    Creazione layout sito

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
-<header>
+{{-- <header> --}}
     {{-- <img class="" src="{{URL::asset('/img/2.jpg')}}" alt=""> --}}
-    <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    {{-- <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img src="https://images.wallpaperscraft.com/image/single/keyboard_backlight_red_136083_2560x1080.jpg" class=" d-block w-100" alt="...">
@@ -46,4 +46,76 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+</header> --}}
+
+ {{-- il carosello da sistemare  Le due foto a sinistra da sistemare--}}
+<header class="container-fluid">
+  <div class="row">
+    <div class="col-9">
+      <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://images.wallpaperscraft.com/image/single/keyboard_backlight_red_136083_2560x1080.jpg" class=" d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block ft">
+                <a href=""><h2 class="animate__animated animate__backInLeft fz text-white">Tastiera meccanica</h2></a>
+                <p class="animate__animated animate__backInLeft fz1">Acquistala Ora</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://images.wallpaperscraft.com/image/single/bmw_car_bw_124257_2560x1080.jpg" class=" d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block ft">
+              <a href=""><h2 class="animate__animated animate__backInRight text-white fz">BMW</h2></a>
+                <p class="animate__animated animate__backInRight fz1">Acquistala Ora</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://images.wallpaperscraft.com/image/single/headphones_bw_headset_120277_2560x1080.jpg" class=" d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block ft">
+              <a href=""><h2 class="animate__animated animate__backInDown text-white fz">Cuffie</h2></a>
+                <p class="animate__animated animate__backInDown fz1">Acquistala Ora</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://images.wallpaperscraft.com/image/single/guitar_guitarist_bw_125985_2560x1080.jpg" class=" d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block ft">
+              <a href=""><h2 class="animate__animated animate__backInDown text-white fz">Guitar</h2></a>
+                <p class="animate__animated animate__backInDown fz1">Acquistala Ora</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://images.wallpaperscraft.com/image/single/auto_black_headlight_119072_2560x1080.jpg" class=" d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block ft">
+              <a href=""><h2 class="animate__animated animate__backInUp text-white fz">Porche</h2></a>
+                <p class="animate__animated animate__backInUp fz1">Acquistala Ora</p>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      </div>
+  </div>
+    <div class="col-3">
+      <div class="row">
+        <div class="col-12"><img src="https://picsum.photos/200" alt=""></div>
+        <div class="col-12"><img src="https://picsum.photos/200" alt=""></div>
+      </div>
+    </div>
+  </div>
 </header>
+
+{{--Le 4 cose strane --}}
+<section class="container-fluid py-5 my-5">
+  <div class="row">
+    <div class="col-3">Qualita product</div>
+    <div class="col-3">Free ship</div>
+    <div class="col-3">senza Ritorno</div>
+    <div class="col-3">Nessun Suppoto</div>
+  </div>
+</section>
+
--- resources/views/components/section-one.blade.php
+++ resources/views/components/section-one.blade.php
@@ -1,4 +1,24 @@
-<div class="container">
+{{-- Section delle card categorie fare modifiche css --}}
+<section class="container-fluid">
+    <h1>Categorie</h1>
+    <div class="row">
+        @foreach ($categories as $category)
+        <div class="col-12 col-md-2 my-4">
+            <div class="card shadow" style="width :8rem ;">
+            <img src="https://picsum.photos/100" alt="Random img" class="cart-img-top p-3 rounded">
+            <a href="{{route('categoryShow', compact('category'))}}" class="dropdown-item"><h3>{{$category->name}}</h3></a>
+            </div>
+        </div>
+            
+        @endforeach
+    </div>
+</section>
+
+
+
+
+{{-- section degli annunci fare modifiche css --}}
+<section class="container">
     <div class="col-12">
         <h2>Annunci In prima vista!</h2>
     </div>
@@ -21,4 +41,14 @@
         @endforeach
     </div>
 
-</div>
+</section>
+
+{{-- section delle due foto inferiori aggiungere scritta --}}
+<section class="container-fluid">
+    <div class="row">
+        <div class="col-6"><img src="https://picsum.photos/1000/300" alt="">
+        <h2>Scritta</h2></div>
+        <div class="col-6"><img src="https://picsum.photos/1000/301" alt="">
+        <h2>Scritta</h2></div>
+    </div>
+</section>
