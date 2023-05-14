5c5dbe64eadce0b132a1b6f823630b3ad8687eb4
AndriyHado <andriycool905@gmail.com>
2023-05-14 16:05:36 +0200

    Login ottimizzato

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,43 @@
 <x-main>
-<div class="container padding-login">
-    <section class="vh-100">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-6 text-black">
+    <x-navbar/>
+        <section class="my-5">
+            <div class="container my-5">
+                    <div class="row  bg-light border border-3 border-dark border-opacity-25 rounded">
+                        <div class="col-12 col-md-6 text-black">
+                            <form action="/login" method="POST"  class="m-5 " >
+                                    @csrf
+                                <h3 class="fw-normal mb-5 text-center text-body">LOGIN FORM</h3>
+                                <div class="form-outline mb-2">
+                                    <input type="email" name="email" id="email" class="form-control form-control-lg" value="{{ old('email') }}"/>
+                                    <label class="form-label m-2" for="email">Email address</label>
+                                    @error('email')
+                                        <span class="small text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
 
-              <div class="px-5 ms-xl-4">
-                <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-                <span class="h1 fw-bold mb-0">Logo</span>
-              </div>
+                                <div class="form-outline mb-2">
+                                    <input type="password"name="password" id="password" class="form-control form-control-lg" value="{{ old('password') }}"/>
+                                    <label class="form-label m-2" for="password">Password</label>
+                                    @error('email')
+                                        <span class="small text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
 
-              <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+                                <div class="pt-1 mb-5 text-center">
+                                    <button class="btn-hover btn btn-warning btn-sm py-2 px-5 " type="submit">Login</button>
+                                </div>
 
-                <form action="/login" method="POST" style="width: 23rem;">
-                    @csrf
-                  <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Login</h3>
+                                <p class="small pb-lg-2"><a class="text-muted" href="#!">Password dimenticata?</a></p>
+                                <p>Non hai un account? <a href="{{route('register')}}" class="link-info">Registrati qui</a></p>
 
-                  <div class="form-outline mb-4">
-                    <input type="email" name="email" id="email" class="form-control form-control-lg" />
-                    <label class="form-label" for="email">Email address</label>
-
-                  </div>
-
-                  <div class="form-outline mb-4">
-                    <input type="password"name="password" id="password" class="form-control form-control-lg" />
-                    <label class="form-label" for="password">Password</label>
-                  </div>
-
-                  <div class="pt-1 mb-4">
-                    <button class="btn btn-warning btn-lg btn-block" type="submit">Login</button>
-                  </div>
-
-                  <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
-                  <p>Don't have an account? <a href="{{route('register')}}" class="link-info">Register here</a></p>
-
-                </form>
-
-              </div>
-
+                            </form>
+                        </div>
+                        <div class="col-md-6 p-0 d-none d-sm-block">
+                            <img src="https://picsum.photos/900/700"
+                                alt="Login image" class="w-100 h-100" >
+                        </div>
+                    </div>
             </div>
-            <div class="col-sm-6 px-0 d-none d-sm-block">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
-                alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
-            </div>
-          </div>
-        </div>
-      </section>
-</div>
-<x-footer/>
+        </section>
+    <x-footer/>
 </x-main>
