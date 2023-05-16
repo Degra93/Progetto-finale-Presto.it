9e6ad9c10516e7b4dd7b703615e730fd7f27fce8
Fabio Zhang <Flasvert@gmail.com>
2023-05-16 21:01:56 +0200

    modifiche annunci, detail, e categorie con card nuove

--- resources/views/categoryShow.blade.php
+++ resources/views/categoryShow.blade.php
@@ -2,7 +2,7 @@
     <div class="spacer"></div>
     <h1 class="text-center">Annunci presenti per la categoria: {{$category->name}}</h1>
 
-    <div class="container ">
+    {{-- <div class="container ">
         <div class="row text-center ">
             @forelse ($category->announcements as $announcement)
             <div class="col-12 col-md-4 pt-4 ps-5 ">
@@ -22,6 +22,44 @@
                 <p>Al momento non esistono annunci per questa categoria...</p>
                 <p>Che ne dici di crearne uno tu? <a href="{{route('announcement.create')}}" class="btn btn-warning">Nuovo annuncio</a></p>
             @endforelse
+        </div>
+    </div> --}}
+    
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                <h6 class="section-title text-center text-primary text-uppercase">Nostri Annunci</h6>
+                <h1 class="mb-5">Explora i nostri  <span class="text-primary text-uppercase">Annunci</span></h1>
+            </div>
+            <div class="row g-4">
+                @forelse ($category->announcements as $announcement)
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                    <div class="room-item shadow rounded overflow-hidden">
+                        <div class="position-relative">
+                            <img class="img-fluid" src="https://picsum.photos/450/300" alt="">
+                            <small class="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">Prezzo: {{$announcement->price}}</small>
+                        </div>
+                        <div class="p-4 mt-2">
+                            <div class="d-flex justify-content-between mb-3">
+                                <h5 class="mb-0">{{$announcement->title}}</h5>
+                            </div>
+                            
+                            <p class="text-body mb-3">{{$announcement->body}}</p>
+                            <div class="d-flex justify-content-between">
+                                <a class="btn btn-sm btn-primary rounded py-2 px-4" href="{{route('announcement.show', compact('announcement'))}}">View Detail</a>
+                                <a class="btn btn-sm btn-dark rounded py-2 px-4" href="{{route('categoryShow',['category'=>$announcement->category])}}">Categoria: {{$announcement->category->name}}</a>
+                                
+                            </div>
+                            <p class="card-footer">Pubblicato il: {{$announcement->created_at->format('d/m/Y')}} Autore: {{$announcement->user->name}}</p>
+                        </div>
+                    </div>
+                </div>
+                @empty
+                <p>Al momento non esistono annunci per questa categoria...</p>
+                <p>Che ne dici di crearne uno tu? <a href="{{route('announcement.create')}}" class="btn btn-warning">Nuovo annuncio</a></p>
+            @endforelse
+               
+            </div>
         </div>
     </div>
 </x-main>
