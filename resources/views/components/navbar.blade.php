76de40e99ac0e7914d7f89f6160ab05452ee3bc2
Matteo <Matteoshurdhaqi@gmail.com>
2023-05-17 19:44:06 +0200

    piccole modifiche, sezione card-slider e header modificate e da modificare

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -246,7 +246,7 @@
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse bg-white" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
                     <li class="nav-item">
