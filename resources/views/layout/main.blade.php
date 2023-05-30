c00f8f79faa97407bc8442b36b6c74e894ec3244
Fabio Zhang <Flasvert@gmail.com>
2023-05-30 20:40:02 +0200

    Font comfortaaaaaah

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -22,9 +22,11 @@ lang="{{ str_replace('_', '-', app()->getLocale()) }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital@1&display=swap" rel="stylesheet">
+        
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">
+        
 
 
         @vite(['resources/css/app.css','resources/js/app.js'])
