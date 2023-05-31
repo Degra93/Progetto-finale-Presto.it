c1706861b8dbb1c010b3428b69a23492382cadf5
Fabio Zhang <Flasvert@gmail.com>
2023-05-31 19:50:12 +0200

    Sistemazione card

--- resources/views/announcement/index.blade.php
+++ resources/views/announcement/index.blade.php
@@ -7,8 +7,8 @@
             <h1 class="mb-5">{{ __('ui.esplora-i') }}</h1>
         </div>
         <div class="row g-4 ">
-            @forelse ($announcements as $announcement)
-                {{-- <img class="postcard__img pt-4" src="{{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path) : 'https://picsum.photos/600'}}" alt="Image Title" /> --}}
+            {{-- @forelse ($announcements as $announcement)
+                <img class="postcard__img pt-4" src="{{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path) : 'https://picsum.photos/600'}}" alt="Image Title" />
 
                 <div class="col-lg-12 col-12 " data-aos="fade-right" data-aos-duration="3000">
 
@@ -44,7 +44,39 @@
 
                     </article>
                 </div>
-            @empty
+            @empty --}}
+            @forelse ($announcements as $announcement)
+            <div class="col-lg-12 col-12  "  data-aos="fade-down-right" data-aos-duration="2000" >
+
+                <article class="postcard light yellow text-center bg-annunci-home" >
+                    <a class="postcard__img_link position-relative" href="#">
+                        <img class="postcard__img "
+                            src="{{ !$announcement->images()->get()->isEmpty()? $announcement->images()->first()->getUrl(1000, 600): 'https://picsum.photos/600' }}"
+                            alt="Image Title" />
+                    </a>
+                    <div class="postcard__text t-dark">
+                        <div class="d-flex justify-content-between">
+
+                      <a href="" class=" btn-warning rounded py-2 px-4 fw-bold">{{__('ui.prezzo')}}:{{$announcement->price}} €</a>
+                  </div>
+                  <h5 class="mb-0 py-2">{{$announcement->title}}</h5>
+                  <div class="postcard__subtitle small">
+                      <p class="card-footer">{{__('ui.pubblicato')}}: {{$announcement->created_at->format('d/m/Y')}} {{__('ui.autore')}}: {{$announcement->user->name}}</p>
+                  </div>
+                  <div class="grande pb-5">
+                  <p class="text-body mb-3 roll">{{$announcement->body}}</p>
+                </div>
+                  <div class="d-flex justify-content-between pt-3">
+                      <a class=" btn-primary rounded pt-2 px-4 fw-bold text-center " href="{{route('announcement.show', compact('announcement'))}}">{{__('ui.dettagli')}}</a>
+                      <a class=" btn-dark rounded py-2 px-4 fw-bold " href="{{route('categoryShow',['category'=>$announcement->category])}}">{{__('ui.categoria')}}: {{__("ui.".$announcement->category->name)}}</a>
+
+                        </div>
+                    </div>
+
+                </article>
+            </div>
+
+        @empty
                 <h2>Non ci sono annunci nella Ricerca</h2>
             @endforelse
         </div>
